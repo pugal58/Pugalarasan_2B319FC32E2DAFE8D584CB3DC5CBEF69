8a928b7b3cabff463859aa14fe2328b021a648c0
pugal58 <144654077+pugal58@users.noreply.github.com>
2023-09-20 07:34:00 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,6 +1,15 @@
-# Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
-year = int(input("Enter a year: "))
-if (year % 4 == 0 and year % 100 != 0)or year%400==0:
-  print(year, "is a leap year")
-else:
-  print(year, "is not a leap year")
+class Player:
+    def play(self):
+        print("The player is playing cricket")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling")
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-#Implement a recursive function to calculate the factorial of a given number.
-def factorial(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*factorial(n-1)
-number=int(input("enter the number:") )
-res= factorial(number)
-print("the factorial of {} is {}".format(number,res))
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}"
+        else:
+            return "Invalid deposit amount. Please enter a positive amount."
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}"
+        elif amount > self.__account_balance:
+            return "Insufficient funds."
+        else:
+            return "Invalid withdrawal amount. Please enter a positive amount."
+
+    def display_balance(self):
+        return f"Account balance for {self.__account_holder_name}: ₹{self.__account_balance}"
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+    account1 = BankAccount("123456789", "pugal", 1000)
+
+    print(account1.display_balance())
+    print(account1.deposit(500))
+    print(account1.withdraw(200))
+    print(account1.withdraw(1500))
+    print(account1.display_balance())
